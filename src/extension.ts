3a75805177cce5d473ed817d9fcd1afa58cc07c0
AkiraEga <akira.ega@gmail.com>
2020-09-28 22:01:01 +0900

    support webview panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { treeCmd } from './treeCmd';
 import { FileTreeItemsProvider, FileItem } from './provider/fileItemProvider';
-import { FileTreeView } from './treeView/fileTreeView';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tree" is now active!');
-	let fileTreeItemsProvider:FileTreeItemsProvider|null = null;
+	let fileTreeItemsProvider: FileTreeItemsProvider | null = null;
 	let fileTreeView: vscode.TreeView<FileItem>;
-	if(vscode.workspace.rootPath){
-		// fileTreeItemsProvider = new FileTreeItemsProvider(vscode.workspace.rootPath);
-		// vscode.window.registerTreeDataProvider('fileTree', fileTreeItemsProvider);
+	if (vscode.workspace.rootPath) {
 		fileTreeItemsProvider = new FileTreeItemsProvider(vscode.workspace.rootPath);
-		fileTreeView = vscode.window.createTreeView('fileTree', {treeDataProvider: fileTreeItemsProvider});
-		fileTreeView.onDidCollapseElement((e: vscode.TreeViewExpansionEvent<FileItem>)=>{
+		fileTreeView = vscode.window.createTreeView('fileTree', { treeDataProvider: fileTreeItemsProvider });
+		fileTreeView.onDidCollapseElement((e: vscode.TreeViewExpansionEvent<FileItem>) => {
 			e.element.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
 		});
-		fileTreeView.onDidExpandElement((e: vscode.TreeViewExpansionEvent<FileItem>)=>{
+		fileTreeView.onDidExpandElement((e: vscode.TreeViewExpansionEvent<FileItem>) => {
 			e.element.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
 		});
 	}
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tree.cmd', (uri:vscode.Uri) => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display tree command result
-		let ret = treeCmd(uri.fsPath);
-		console.log(ret);
-	});
-	context.subscriptions.push(disposable);
-
-	disposable = vscode.commands.registerCommand('tree.cmd.fromFileTreeView', (fileItem: FileItem) => {
-		// The code you place here will be executed every time your command is executed
-
-		if(fileTreeItemsProvider){
-			let ret:string = fileTreeItemsProvider.treeCmd(fileItem,0);
+	let disposable = vscode.commands.registerCommand('tree.cmd', (fileItem: FileItem) => {
+		if (fileTreeItemsProvider) {
+			let ret: string = fileTreeItemsProvider.treeCmd(fileItem, 0);
 			console.log(ret);
+			let retForHtml: string = escape(ret);
+			const panel = vscode.window.createWebviewPanel(
+				'treePreview',
+				`Tree from \"${fileItem.fullPath}\"`,
+				vscode.ViewColumn.Beside,
+				{}
+			);
+			const marked = require('marked');
+			let mdTxt:string =
+`
+### File Tree
+\`\`\`bash
+${ret}
+\`\`\`
+`;
+			panel.webview.html = marked(mdTxt);
+
 		}
 
 	});
@@ -51,4 +44,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
